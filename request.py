a862173d2fe60bac68adbd7b94dcbb350f18024f
EvanJustBeGood <Melnic.ivan2005@gmail.com>
2023-08-10 14:25:04 +0200

    Update request.py

--- request.py
+++ request.py
@@ -1,11 +1,16 @@
-import request
+import requests
 
-url = "https://github.com/EvanJustBeGood/Python-hellow-World"
-response = requests.get(url)
+def get_repository_names(username):
+    url = f"https://api.github.com/users/EvanJustBeGood/repos"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        repositories = response.json()
+        for repo in repositories:
+            print(repo["name"])
+    else:
+        print(f"Failed to retrieve repositories for user {username}. Status code: {response.status_code}")
 
-
-if response.status_code == 200:
-    data = response.json()  
-    print(data)
-else:
-    print("Ошибка запроса:", response.status_code)
+if __name__ == "__main__":
+    username = "EvanJustBeGood"
+    get_repository_names(username)
